ca24725521f3826836f3c3b2a575ba02e159375e
Anshuman Joshi <126323808+godfatherspie@users.noreply.github.com>
2023-09-19 01:40:26 +0530

    Update tut10.py

--- tut10.py
+++ tut10.py
@@ -1,3 +1,6 @@
+#Create a new list from two list using the following condition
+#Given two list of numbers, write a program to create a new list such that the new list should contain odd numbers from the first list and even numbers from the second list.
+
 list1 = [10,20,33,41,50]
 list2 = [20,40,60,70,89]
 res_list = []
