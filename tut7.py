f4cecef32152d38b9d47c9b4ed1a214a1ecd3475
Anshuman Joshi <126323808+godfatherspie@users.noreply.github.com>
2023-09-19 01:38:32 +0530

    Update tut7.py

--- tut7.py
+++ tut7.py
@@ -1,3 +1,5 @@
+#Return the count of a given substring from a string
+
 str1 = "My first name is Anshuman. My full name is Anshuman Jaydeep Joshi."
 print("Sentence: "+str1)
 print("Anshuman is repeated "+str(str1.count("Anshuman"))+" times")
